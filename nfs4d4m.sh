b58e1306de7fb55f56947a4a0269aed9a385f91b
Keith Bentrup <keith@keithbentrup.com>
2017-10-31 21:19:51 -0400

    support ~ paths

--- nfs4d4m.sh
+++ nfs4d4m.sh
@@ -5,6 +5,7 @@
 # turn on debugging
 #set -x
 
+local_username=$(logname)
 
 if [ ! -f "$1" ]; then
    echo "$1 is not a regular file" && exit 1
@@ -17,22 +18,24 @@ mkdir -p .tmp || :
 # clean up and standardize mounts file format
 # remove leading & trailing whitespace, extra "/"s, empty & duplicate lines, append ':' to lines with only a local path
 while read line; do echo -e "$line"; done < "${mount_file}" | 
-  sed '/#.*/d;s/\/*$//;s/\/*:/:/;/^$/d;/:/!s/$/:/' |
+  sed '/ *#.*/d;s/\/*$//;s/\/*:/:/;/^$/d;/:/!s/$/:/' |
   sort | uniq > .tmp/mounts
 
 # check local paths exist and update local paths to explicit paths
 line_num=0
 for local_path in $(sed 's/:.*//' .tmp/mounts | sort); do
   ((line_num++))
-  if ! $(cd "${local_path}"); then
-    echo "Error: '${local_path}' does not exist;" && exit 1
+  eval "local_path=${local_path}"
+  if ! $(cd ${local_path}); then
+    echo "Error: ${local_path} does not exist;" && exit 1
   fi
   resolved_local_path=$( cd "${local_path}"; pwd -P )
   sed -i.bak "${line_num}s|.*:|${resolved_local_path}:|" .tmp/mounts
 done
 
 # if remote path omitted; use same path remotely
-awk -F ':' '{print $1 ":" (($2=="") ? $1 : $2)}' .tmp/mounts > .tmp/awk && mv .tmp/awk .tmp/mounts
+# also remove any duplicates resulting from transformations
+awk -F ':' '{print $1 ":" (($2=="") ? $1 : $2)}' .tmp/mounts | sort | uniq > .tmp/awk && mv .tmp/awk .tmp/mounts
 
 prev_path="this-is-a-fake-path"
 for local_path in $(sed 's/:.*//' .tmp/mounts | sort); do
@@ -55,9 +58,8 @@ done
 perl -i -pe 'BEGIN{undef $/;} s/# d4m.*# d4m\n?//sm' /etc/exports
 
 # add new d4m exports
-logname=$(logname)
-nfs_uid=$(id -u "${logname}")
-nfs_gid=$(id -g "${logname}")
+nfs_uid=$(id -u "${local_username}")
+nfs_gid=$(id -g "${local_username}")
 for local_path in $(sed 's/:.*//' .tmp/mounts); do
     local_path=$( cd "${local_path}"; pwd -P )
     exports="${exports}\n${local_path} -alldirs -mapall=${nfs_uid}:${nfs_gid} localhost"
